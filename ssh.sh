68049ea9a0c203e1812d4d4c04b8315bcfb3181e
more66i <121669223+more66i@users.noreply.github.com>
2022-12-30 19:55:43 +0800

    Update ssh.sh

--- ssh.sh
+++ ssh.sh
@@ -14,4 +14,12 @@ echo "
 最后输入命令：ssh -T git@github.com
 
 Hi xxx! You’ve successfully authenticated, but GitHub does not provide shell access.
+
+如果运行git push origin main之后，出现Everything up-to-date
+运行git commit -m 'name'
+
+如果还是出现..密码..更改..in 2021..
+
+运行git config credential.helper store命令
+再重新推送文件
 "
